6393d7c2e7624a6449b4c8a50158d693d898bc28
Dmitry Orlov <dimorlus@gmail.com>
2025-11-06 21:28:02 +0200

    06-Nov-25 21:28:02.51

--- build.bat
+++ build.bat
@@ -1,6 +1,13 @@
 @echo off
-echo Building RegExpCalc...
+echo Building RegExpCalc64...
 "C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" RegExpCalc.sln /p:Configuration=Debug /p:Platform=x64 /t:Rebuild /v:minimal
+if %ERRORLEVEL% EQU 0 (
+    echo Build succeeded!
+) else (
+    echo Build failed with error %ERRORLEVEL%
+)
+echo Building RegExpCalc32...
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\MSBuild\Current\Bin\MSBuild.exe" RegExpCalc.sln /p:Configuration=Debug /p:Platform=x86 /t:Rebuild /v:minimal
 if %ERRORLEVEL% EQU 0 (
     echo Build succeeded!
 ) else (
